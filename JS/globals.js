ea98e787261f59266e1bf45c8b33dd7c58d05dc8
Ezeilo Ugonna <ezeilougonna10@gmail.com>
2024-09-24 16:42:09 +0100

    refactor
    - update color variables
    - update header styles and structure

--- JS/globals.js
+++ JS/globals.js
@@ -2,11 +2,3 @@
 export const hamburger = document.querySelector('.hamburger');
 export const headerMenu = document.querySelector('.header__menu');
 export const headerBackdrop = document.querySelector('.header__backdrop');
-
-export const headerSelectBtn = document.querySelector('.header__select-btn');
-export const headerDropdownMenu = document.querySelector('.header__dropdown-menu');
-
-
-export const headerDropdown = document.querySelector('.header__dropdown');
-export const headerSelectBtnIconWrapper = document.querySelector('.header__select-btn-icon-wrapper');
-export const headerSelectBtnText = document.querySelector('.header__select-btn-text');
--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,5 @@
 import { toggleMobileMenuOnHamburgerClick, closeMobileMenuOnOutsideClick, closeMobileMenuOnLinkClick } from "./navbar/toggleMenu.js";
-import { toggleDropdownOnHeaderClick } from "./navbar/dropDown.js";
 
 toggleMobileMenuOnHamburgerClick();
 closeMobileMenuOnOutsideClick();
 closeMobileMenuOnLinkClick();
-toggleDropdownOnHeaderClick();
